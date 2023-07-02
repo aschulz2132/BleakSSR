7b6cddc2c9796dae509e0d66ba64d769fdcfb5fe
aliceschulz <aschulz2132@gmail.com>
2023-07-02 12:46:19 -0700

    first!

---
+++ src/routes/userContatiner.svelte
@@ -0,0 +1,17 @@
+
+<style>
+    .userContainerMainPage {
+        background-color: whitesmoke;
+    }
+    .userContainerMainHeader {
+        
+    }
+    ]
+</style>
+
+<div class="userContainerMainPage">
+    <div class="userContainerMainHeader"> 
+        <p> e</p>
+    </div>
+
+</div>
